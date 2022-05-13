ea6eda34e7f439b7a144ad351aedd50da2ee4793
gaston.ara <gaston.ara.o@gmail.com>
2022-05-13 03:22:31 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [ingredientes, setIngredientes] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let focusName = e.target.children[1];
+     const newCoctel = await axios.post('http://localhost:4000/cocteles', {
+       nombre: name,
+       ingredientes: ingredientes,
+       category: category
+     })
+     console.log(newCoctel.data);
+    setName('');
+    setIngredientes('');
+     focusName.focus();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='form' onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}/>
+        <input type="text" placeholder="Nombre" value={name} onChange={(e) => setName(e.target.value)}/>
+        <textarea type="text" placeholder="Ingredientes" value={ingredientes} onChange={(e) => setIngredientes(e.target.value)}/>
+        <button type="submit">Agregar</button>
+      </form>
     </div>
   );
 }
